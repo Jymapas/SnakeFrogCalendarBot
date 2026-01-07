1a4bd9fff8ee64c99ceade3178ba2e18ab01e5ed
Aleksandr Petrakov <a.petrakov@tha.kz>
2026-01-07 19:33:33 +0500

    chore(Worker): add Serialog

--- src/SnakeFrogCalendarBot.Worker/Program.cs
+++ src/SnakeFrogCalendarBot.Worker/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Serilog;
 
-Console.WriteLine("Hello, World!");
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/worker-.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+try
+{
+    Log.Information("SnakeFrogCalendarBot.Worker starting");
+    Console.WriteLine("Hello, World!");
+    Log.Information("SnakeFrogCalendarBot.Worker stopping");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
