644c99fa22d75e441542247543c199299fb1a1a8
Aleksandr Petrakov <a.petrakov@tha.kz>
2026-01-11 00:05:05 +0500

    chore: update ConversationNames

--- src/SnakeFrogCalendarBot.Worker/Telegram/ConversationNames.cs
+++ src/SnakeFrogCalendarBot.Worker/Telegram/ConversationNames.cs
@@ -3,6 +3,8 @@ namespace SnakeFrogCalendarBot.Worker.Telegram;
 public static class ConversationNames
 {
     public const string BirthdayAdd = "birthday-add";
+    public const string BirthdayEdit = "birthday-edit";
     public const string EventAdd = "event-add";
+    public const string EventEdit = "event-edit";
     public const string WaitingForEventFile = "waiting-for-event-file";
 }
