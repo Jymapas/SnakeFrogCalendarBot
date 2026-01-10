433b284ffc7b22fc1b21bcb7545ab2370ffdd2a3
Aleksandr Petrakov <a.petrakov@tha.kz>
2026-01-11 00:04:23 +0500

    feat(Worker): add EventEditConversationSteps

---
+++ src/SnakeFrogCalendarBot.Worker/Telegram/EventEditConversationSteps.cs
@@ -0,0 +1,12 @@
+namespace SnakeFrogCalendarBot.Worker.Telegram;
+
+public static class EventEditConversationSteps
+{
+    public const string Title = "title";
+    public const string Date = "date";
+    public const string AllDay = "all-day";
+    public const string Time = "time";
+    public const string Description = "description";
+    public const string Place = "place";
+    public const string Link = "link";
+}
